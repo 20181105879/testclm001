4bc9612460c05ef61a31dc93dd2c1d0a5de4be9d
20181105879 <2605753145@qq.com>
2019-06-19 21:38:55 +0800

    Initial Commit

---
+++ testclm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testclm
+//
+//  Created by s20181105879 on 2019/6/19.
+//  Copyright © 2019 s20181105879. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
